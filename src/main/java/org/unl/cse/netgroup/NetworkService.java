977c4eb411e04c4de4847df41ad12c7d4ce99f0e
Deepak Nadig Anantha <admin@deepaknadig.com>
2016-07-14 02:46:52 -0500

    Updated NetworkService

--- src/main/java/org/unl/cse/netgroup/NetworkService.java
+++ src/main/java/org/unl/cse/netgroup/NetworkService.java
@@ -9,11 +9,50 @@ import java.util.Set;
  * Created by dna on 7/12/16.
  */
 public interface NetworkService extends ListenerService<NetworkEvent, NetworkListener> {
-    void createNetwork (String network);
 
+    /**
+     * Create a named network.
+     *
+     * @param network network name
+     */
+    void createNetwork(String network);
+
+    /**
+     * Deletes a named network.
+     *
+     * @param network network name
+     */
     void deleteNetwork(String network);
 
+    /**
+     * Returns a set of network names.
+     *
+     * @return a set of network names
+     */
     Set<String> getNetworks();
 
+    /**
+     * Adds a host to the given network.
+     *
+     * @param network network name
+     * @param hostId host id
+     */
     void addHost(String network, HostId hostId);
+
+    /**
+     * Removes a host from the given network.
+     *
+     * @param network network name
+     * @param hostId host id
+     */
+    void removeHost(String network, HostId hostId);
+
+    /**
+     * Returns all the hosts in a network.
+     *
+     * @param network network name
+     * @return set of host ids
+     */
+    Set<HostId> getHosts(String network);
+
 }

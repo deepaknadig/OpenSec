0688069f57d422eefdb8a9f3cc94a3e7bf80f303
Deepak Nadig Anantha <admin@deepaknadig.com>
2016-08-14 23:51:04 -0500

    Updated TCP Packet Processor

--- src/main/java/org/unl/cse/netgroup/TcpProcessor.java
+++ src/main/java/org/unl/cse/netgroup/TcpProcessor.java
@@ -8,6 +8,7 @@ import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.ReferenceCardinality;
 import org.onlab.packet.Ethernet;
 import org.onlab.packet.IPv4;
+import org.onlab.packet.IpAddress;
 import org.onlab.packet.MacAddress;
 import org.onosproject.core.ApplicationId;
 import org.onosproject.core.CoreService;
@@ -28,6 +29,7 @@ import org.slf4j.LoggerFactory;
 
 import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
 import java.util.Timer;
 
 /**
@@ -40,7 +42,6 @@ public class TcpProcessor {
     private static Logger logger = LoggerFactory.getLogger(TcpProcessor.class);
 
     private static final String MSG_TCP_TRANSMISSION = "TCP Packet Transmission detected between " + "{} and {} by {}";
-    private static final String MSG_TCP_TRANSMISSION2 = "TCP2";
 
     @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
     protected CoreService coreService;
@@ -63,11 +64,13 @@ public class TcpProcessor {
             .matchEthType(Ethernet.TYPE_IPV4).matchIPProtocol(IPv4.PROTOCOL_TCP)
             .build();
 
-    private final HashMultimap<DeviceId, TcpRecord> tcps = HashMultimap.create();
+    private final HashMultimap<DeviceId, TcpRecord> tcpHashMultimap = HashMultimap.create();
     private final Timer timer = new Timer("tcp-sweeper");
 
     private static final int PRIORITY = 128;
     private static final int DROP_PRIORITY = 129;
+    private long tempCountHolder = 0;
+
 
     @Activate
     public void activate() {
@@ -90,45 +93,78 @@ public class TcpProcessor {
     }
 
     // Process TCP Packets
-    private void processTcp(PacketContext context, Ethernet ethernet) {
+    private void processTcp(PacketContext context, Ethernet ethernet, boolean packetIncrement) {
         DeviceId deviceId = context.inPacket().receivedFrom().deviceId();
         MacAddress src = ethernet.getSourceMAC();
         MacAddress dst = ethernet.getDestinationMAC();
-        TcpRecord tcp = new TcpRecord(src, dst);
-        boolean tcped = tcps.get(deviceId).contains(tcp);
-//        logger.info(String.valueOf(tcped));
+        long pktCount = 0;
 
-        if (tcped) {
-            // TCP packet transmissions detected
-            logger.warn(MSG_TCP_TRANSMISSION, new Object[]{src, dst, deviceId});
-            installTcpRules(); // TODO: Update the rule installation
+        IPv4 ippayload = (IPv4) ethernet.getPayload();
+        int srcaddr = ippayload.getSourceAddress();
+        int dstaddr = ippayload.getDestinationAddress();
+        IpAddress srcip = IpAddress.valueOf(srcaddr);
+        IpAddress dstip = IpAddress.valueOf(dstaddr);
+
+
+        TcpRecord tcpRecord = new TcpRecord(src, dst, pktCount);
+        boolean tcpRecordExists = tcpHashMultimap.get(deviceId).contains(tcpRecord);
+
+        if (tcpRecordExists) {
+            // TCP packet transmissions detected, Perform an action on the detected flow.
+            logger.debug(MSG_TCP_TRANSMISSION, new Object[]{src, dst, deviceId});
+            if (packetIncrement) {
+                tempCountHolder += 1;
+                tcpRecord.pktCount = tempCountHolder;
+
+                tcpHashMultimap.remove(deviceId, tcpRecord);
+                tcpHashMultimap.put(deviceId, tcpRecord);
+
+            }
+
+            installFlowRules(); // TODO: Update the rule installation
+
+            Set<TcpRecord> recordReader;
+            recordReader = tcpHashMultimap.get(deviceId);
+
+            for(TcpRecord record : recordReader){
+                logger.info(" Source: " + record.src
+                                    + " Dest: " + record.dst
+                                    + " PktCount: " + record.pktCount);
+            }
+
         }
         else {
-            // Do something
-            logger.warn(MSG_TCP_TRANSMISSION2, new Object[]{src, dst, deviceId});
+            // Otherwise, Add TCP Record to the HashMultiMap
+            logger.debug(MSG_TCP_TRANSMISSION, new Object[]{srcip, dstip, deviceId});
+            tcpHashMultimap.put(deviceId, tcpRecord);
         }
     }
 
     // Install temporary rule for the flow (?) for TCP transmisssions between the src and dst.
-    private void installTcpRules() {
+    private void installFlowRules() {
         // TODO: create a TCP flow rule installer for the src/dst pair
     }
 
     // Checks whether the packet is a specified TCP packet
     private boolean isTcpPacket(Ethernet ethernet) {
 //        logger.info(String.valueOf(((IPv4) ethernet.getPayload()).getProtocol()));
-        return ethernet.getEtherType() == Ethernet.TYPE_IPV4 &&
-                ((IPv4) ethernet.getPayload()).getProtocol() == IPv4.PROTOCOL_TCP;
+        int payloadLength = ((IPv4) ethernet.getPayload()).getTotalLength();
+        if (payloadLength >= 64 && ((IPv4) ethernet.getPayload()).getProtocol() == IPv4.PROTOCOL_TCP) {
+            return ethernet.getEtherType() == Ethernet.TYPE_IPV4;
+        }
+        return false;
 
     }
 
     private class TcpRecord {
         private final MacAddress src;
         private final MacAddress dst;
+        private long pktCount;
 
-        public TcpRecord(MacAddress src, MacAddress dst) {
+        public TcpRecord(MacAddress src, MacAddress dst, long pktCount) {
             this.src = src;
             this.dst = dst;
+            this.pktCount = pktCount;
         }
 
         @Override
@@ -150,12 +186,14 @@ public class TcpProcessor {
     }
 
     private class TcpPacketProcessor implements PacketProcessor {
+        private boolean packetCountIncr = false;
 
         @Override
         public void process(PacketContext context) {
             Ethernet ethernet = context.inPacket().parsed();
             if (isTcpPacket(ethernet)) {
-                processTcp(context, ethernet);
+                packetCountIncr = true;
+                processTcp(context, ethernet, packetCountIncr);
             }
         }
     }
